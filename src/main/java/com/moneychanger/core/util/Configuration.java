5da964407bb4aff352efc26d6c4a76782c31b7c0
Cameron Garnham <da2ce7@gmail.com>
2012-04-23 00:12:59 +1000

    new configBean, used in new loader

--- src/main/java/com/moneychanger/core/util/Configuration.java
+++ src/main/java/com/moneychanger/core/util/Configuration.java
@@ -1,92 +1,76 @@
-/************************************************************
------BEGIN PGP SIGNED MESSAGE-----
-Hash: SHA256
-
- *                 M O N E Y C H A N G E R
+/**
+ * **********************************************************
+ * -----BEGIN PGP SIGNED MESSAGE----- Hash: SHA256
  *
- *  Open Transactions:
- *       Financial Cryptography and Digital Cash
- *       Library, Protocol, API, Server, and GUI 
- *    
- *    	 -- Anonymous Numbered Accounts.
- *    	 -- Untraceable Digital Cash.
- *    	 -- Triple-Signed Receipts.
- *    	 -- Cheques, Vouchers, Transfers, Inboxes.
- *    	 -- Basket Currencies, Markets, Payment Plans.
- *    	 -- Signed, XML, Ricardian-style Contracts.
- *    
- *  Copyright (C) 2010-2012 by "Fellow Traveler" (A pseudonym)
+ * M O N E Y C H A N G E R
  *
- *  EMAIL:
- *  FellowTraveler@rayservers.net
- *  
- *  FINGERPRINT:
- *  9DD5 90EB 9292 4B48 0484  7910 0308 00ED F951 BB8E
+ * Open Transactions: Financial Cryptography and Digital Cash Library, Protocol,
+ * API, Server, and GUI
  *
- *  BITCOIN:  1NtTPVVjDsUfDWybS4BwvHpG2pdS9RnYyQ
+ * -- Anonymous Numbered Accounts. -- Untraceable Digital Cash. -- Triple-Signed
+ * Receipts. -- Cheques, Vouchers, Transfers, Inboxes. -- Basket Currencies,
+ * Markets, Payment Plans. -- Signed, XML, Ricardian-style Contracts.
  *
- *  OFFICIAL PROJECT WIKI:
- *  https://github.com/FellowTraveler/Moneychanger
- *  https://github.com/FellowTraveler/Open-Transactions/wiki
+ * Copyright (C) 2010-2012 by "Fellow Traveler" (A pseudonym)
  *
- *  WEBSITE:
- *  http://www.OpenTransactions.org/
- *    
- *  Components and licensing:
- *   -- Moneychanger..A Java client GUI.....LICENSE:.....GPLv3
- *   -- OTLib.........A class library.......LICENSE:...LAGPLv3 
- *   -- OT-API........A client API..........LICENSE:...LAGPLv3
- *   -- testwallet....Command-line client...LICENSE:...LAGPLv3
- *   -- OT-Server.....Server Application....LICENSE:....AGPLv3
- *  Github.com/FellowTraveler/Open-Transactions/wiki/Components
+ * EMAIL: FellowTraveler@rayservers.net
  *
- *  All of the above OT components were designed and written by
- *  Fellow Traveler, with the exception of Moneychanger, which
- *  was contracted out to Vicky C (livewire_3001@yahoo.com).
+ * FINGERPRINT: 9DD5 90EB 9292 4B48 0484 7910 0308 00ED F951 BB8E
  *
- *  -----------------------------------------------------
+ * BITCOIN: 1NtTPVVjDsUfDWybS4BwvHpG2pdS9RnYyQ
  *
- *   LICENSE:
- *   This program is free software: you can redistribute it
- *   and/or modify it under the terms of the GNU General
- *   Public License as published by the Free Software
- *   Foundation, either version 3 of the License, or (at your
- *   option) any later version.
+ * OFFICIAL PROJECT WIKI: https://github.com/FellowTraveler/Moneychanger
+ * https://github.com/FellowTraveler/Open-Transactions/wiki
  *
- *   You should have received a copy of the GNU General
- *   Public License along with this program.  If not, see:
- *   http://www.gnu.org/licenses/
+ * WEBSITE: http://www.OpenTransactions.org/
  *
- *   If you would like to use this software outside of the free
- *   software license, please contact FellowTraveler.
- *   (Unfortunately many will run anonymously and untraceably,
- *   so who could really stop them?)
- *   
- *   DISCLAIMER:
- *   This program is distributed in the hope that it will be
- *   useful, but WITHOUT ANY WARRANTY; without even the implied
- *   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- *   PURPOSE.  See the GNU General Public License for more
- *   details.
-
------BEGIN PGP SIGNATURE-----
-Version: GnuPG v1.4.11 (Darwin)
-
-iQIcBAEBCAAGBQJOj7gfAAoJEAMIAO35UbuOs3sP/2rrjSdYu/AsXcgLK9/9CP4a
-lIJfw3KLvybKZjZW5r5j+4xUlCYIqPZSI66PGDChGPMPFcZQN6M4Ddpn9kbctymS
-sdTXvbdFhpbV6k2wSa1Fz97ygfXJc/7MDTmHYbZ53hVV8AoUBrCHWtVttkQD31o3
-Pn/qGmy+jOgTvjEXhjEpV66pDkMWze1SiI1MArHUziCYoxItuM45x0EfzwQIqlo3
-ku2R7rRTtqm47Dgea12psWrjbPS5XRL1Q8Hs38Z1J0JdFlfn6cJYe52Iiluzof6M
-kCLhy6FH8QfIADfrKkFP48EIhnVquDlkV9AlJ1r217K3cpK2jEjlZUnGBECMAMEo
-pSXXk1BLNgxsa4yaXCgHY92/MhgtcdCMLkcCq6MWUTGZsLGiWIiQGmO9mwBfNIlY
-SawlIviuS5DiE/D16A290Byxhha/5e144cIiKm27fSQra8eogUXNfZdZeuv6n69v
-t8QjeBjoLhe5/KnRNoGLpSXhPphsWLRSJBru77ZU2msHfmkNfcP2UoEUCfNTfTbE
-XpyRfeyRVowVKeKunV9KUSHgdD5wa6RUeyodAbaHvWrFpIpNkaFIP9OwhRULpjx0
-arwVNYucbX1qb2I8HBm2u+IRWQTONp74TFFjU0/CVAXu2DeJKY5mL4zDej35c5j9
-AK+ZirdWhhoHeWR1tAkN
-=RcXP
------END PGP SIGNATURE-----
- **************************************************************/
+ * Components and licensing: -- Moneychanger..A Java client
+ * GUI.....LICENSE:.....GPLv3 -- OTLib.........A class
+ * library.......LICENSE:...LAGPLv3 -- OT-API........A client
+ * API..........LICENSE:...LAGPLv3 -- testwallet....Command-line
+ * client...LICENSE:...LAGPLv3 -- OT-Server.....Server
+ * Application....LICENSE:....AGPLv3
+ * Github.com/FellowTraveler/Open-Transactions/wiki/Components
+ *
+ * All of the above OT components were designed and written by Fellow Traveler,
+ * with the exception of Moneychanger, which was contracted out to Vicky C
+ * (livewire_3001@yahoo.com).
+ *
+ * -----------------------------------------------------
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see: http://www.gnu.org/licenses/
+ *
+ * If you would like to use this software outside of the free software license,
+ * please contact FellowTraveler. (Unfortunately many will run anonymously and
+ * untraceably, so who could really stop them?)
+ *
+ * DISCLAIMER: This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * -----BEGIN PGP SIGNATURE----- Version: GnuPG v1.4.11 (Darwin)
+ *
+ * iQIcBAEBCAAGBQJOj7gfAAoJEAMIAO35UbuOs3sP/2rrjSdYu/AsXcgLK9/9CP4a
+ * lIJfw3KLvybKZjZW5r5j+4xUlCYIqPZSI66PGDChGPMPFcZQN6M4Ddpn9kbctymS
+ * sdTXvbdFhpbV6k2wSa1Fz97ygfXJc/7MDTmHYbZ53hVV8AoUBrCHWtVttkQD31o3
+ * Pn/qGmy+jOgTvjEXhjEpV66pDkMWze1SiI1MArHUziCYoxItuM45x0EfzwQIqlo3
+ * ku2R7rRTtqm47Dgea12psWrjbPS5XRL1Q8Hs38Z1J0JdFlfn6cJYe52Iiluzof6M
+ * kCLhy6FH8QfIADfrKkFP48EIhnVquDlkV9AlJ1r217K3cpK2jEjlZUnGBECMAMEo
+ * pSXXk1BLNgxsa4yaXCgHY92/MhgtcdCMLkcCq6MWUTGZsLGiWIiQGmO9mwBfNIlY
+ * SawlIviuS5DiE/D16A290Byxhha/5e144cIiKm27fSQra8eogUXNfZdZeuv6n69v
+ * t8QjeBjoLhe5/KnRNoGLpSXhPphsWLRSJBru77ZU2msHfmkNfcP2UoEUCfNTfTbE
+ * XpyRfeyRVowVKeKunV9KUSHgdD5wa6RUeyodAbaHvWrFpIpNkaFIP9OwhRULpjx0
+ * arwVNYucbX1qb2I8HBm2u+IRWQTONp74TFFjU0/CVAXu2DeJKY5mL4zDej35c5j9
+ * AK+ZirdWhhoHeWR1tAkN =RcXP -----END PGP SIGNATURE-----
+ *************************************************************
+ */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -99,76 +83,67 @@ package com.moneychanger.core.util;
  */
 public class Configuration {
 
-    private static long waitTime = 1;
-    private static int retryCount = 1;
-    private static int nbrTransactionCount = 10;
-    private static String rippleUsernameID = "username";
-    private static String ripplePasswordID = "password";
-    private static String loomPasswordID = "passphrase";
-    private static String imagePath = null;
-
     public static String getImagePath() {
-        return imagePath;
+        return ConfigBean.Static.getKey(ConfigBean.Static.Keys.PasswordImagePath);
     }
 
     public static void setImagePath(String imagePath) {
-        Configuration.imagePath = imagePath;
+        ConfigBean.Static.setKey(ConfigBean.Static.Keys.PasswordImagePath, imagePath);
     }
 
     public static String getLoomPasswordID() {
-        return loomPasswordID;
+        return ConfigBean.Static.getKey(ConfigBean.Static.Keys.LoomPasswordID);
     }
 
     public static void setLoomPasswordID(String loomPasswordID) {
-        Configuration.loomPasswordID = loomPasswordID;
+        ConfigBean.Static.setKey(ConfigBean.Static.Keys.LoomPasswordID, loomPasswordID);
     }
-    private static String marketMaxDepth = "100";
 
     public static String getMarketMaxDepth() {
-        return marketMaxDepth;
+        return ConfigBean.Static.getKey(ConfigBean.Static.Keys.MarketMaxDepth);
     }
 
     public static void setMarketMaxDepth(String marketMaxDepth) {
-        Configuration.marketMaxDepth = marketMaxDepth;
+        ConfigBean.Static.setKey(ConfigBean.Static.Keys.MarketMaxDepth, marketMaxDepth);
     }
 
     public static String getRipplePasswordID() {
-        return ripplePasswordID;
+        return ConfigBean.Static.getKey(ConfigBean.Static.Keys.RipplePasswordID);
     }
 
     public static void setRipplePasswordID(String ripplePasswordID) {
-        Configuration.ripplePasswordID = ripplePasswordID;
+        ConfigBean.Static.setKey(ConfigBean.Static.Keys.RipplePasswordID, ripplePasswordID);
     }
 
     public static String getRippleUsernameID() {
-        return rippleUsernameID;
+        return ConfigBean.Static.getKey(ConfigBean.Static.Keys.RippleUsernameID);
     }
 
     public static void setRippleUsernameID(String rippleUsernameID) {
-        Configuration.rippleUsernameID = rippleUsernameID;
+        ConfigBean.Static.setKey(ConfigBean.Static.Keys.RippleUsernameID, rippleUsernameID);
     }
 
     public static int getNbrTransactionCount() {
-        return nbrTransactionCount;
+        return ConfigBean.Static.getKeyAsInteger(ConfigBean.Static.Keys.NbrTransactionCount);
     }
 
     public static void setNbrTransactionCount(int nbrTransactionCount) {
-        Configuration.nbrTransactionCount = nbrTransactionCount;
+        ConfigBean.Static.setGenericKey(ConfigBean.Static.Keys.NbrTransactionCount, nbrTransactionCount);
     }
 
     public static int getRetryCount() {
-        return retryCount;
+        return ConfigBean.Static.getKeyAsInteger(ConfigBean.Static.Keys.RetryCount);
     }
 
     public static void setRetryCount(int retryCount) {
-        Configuration.retryCount = retryCount;
+        ConfigBean.Static.setGenericKey(ConfigBean.Static.Keys.RetryCount, retryCount);
     }
 
     public static long getWaitTime() {
-        return waitTime;
+        return ConfigBean.Static.getKeyAsLong(ConfigBean.Static.Keys.WaitTime);
     }
 
     public static void setWaitTime(long waitTime) {
-        Configuration.waitTime = waitTime;
+        ConfigBean.Static.setGenericKey(ConfigBean.Static.Keys.WaitTime, waitTime);
     }
 }
